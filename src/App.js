a2ecd903a54d94ffe88ad253244f56de0bf7de37
echizenyayota <virtualmisso30@gmail.com>
2023-12-23 18:36:37 +0900

    4:11 Place the necessary codes in the search-section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 const App = () => {
+  const surpriseOptions = [
+    "A blue ostrich eating melon",
+    "A matisse style shark on the telephone",
+    "A pineapple sunbathing on an island",
+  ];
+
   return (
     <div className="app">
+      <section className="search-section">
+        <p>Start with a detailed description 
+          <span className="surprise"> Surprise me</span>
+        </p>
+        <div className="input-container">
+          <input 
+            placeholder="An impression oil painting of a sunflower in a purple vase..."
+          />
+          <button>Generate</button>
 
+        </div>
+      </section>
+      <section className="image-section"></section>
     </div>
   );
 }
