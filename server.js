c1246f95be05a5886d1359664a09fd2ee5fe7606
echizenyayota <virtualmisso30@gmail.com>
2023-12-23 22:25:04 +0900

    24:44 Set the route to post images in the backend

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const PORT = 8000;
+
+const OpenAI = require("openai");
 const express = require("express");
 
 const cors = require("cors");
+
+const openai = new OpenAI();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +16,19 @@ app.get("/", (req, res) => {
     message: "Testing whether the API works"
   });
 }); 
+
+app.post("/images", async(req, res) => {
+  try {
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: "A cute baby sea otter"
+    });
+    console.log(response.data);
+    res.send(response.data);
+  } catch(error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT));
 
