1d789258418c5435257d73d1af7e410e3ea792de
echizenyayota <virtualmisso30@gmail.com>
2023-12-25 19:05:43 +0900

    33:38 Map out the images and get the value in the input element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [images, setImages] = useState(null);
+  const [value, setValue] = useState(null);
+
   const surpriseOptions = [
     "A blue ostrich eating melon",
     "A matisse style shark on the telephone",
@@ -19,10 +25,13 @@ const App = () => {
       const response = await fetch('http://localhost:8000/images', options);
       const data = await response.json();
       console.log(data);
+      setImages(data);
     } catch(error) {
       console.error(error);
     }
   }
+
+  console.log("value", value);
 
   return (
     <div className="app">
@@ -32,13 +41,19 @@ const App = () => {
         </p>
         <div className="input-container">
           <input 
+            value={value}
             placeholder="An impression oil painting of a sunflower in a purple vase..."
+            onChange={(e) => setValue(e.target.value)}
           />
           <button onClick={getImages}>Generate</button>
 
         </div>
       </section>
-      <section className="image-section"></section>
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img key={_index} src={image.url} alt={`Generated image of ${value}`} />
+        ))}
+      </section>
     </div>
   );
 }
