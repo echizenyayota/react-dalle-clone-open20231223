3f8aa4b8d5420e75a98358b00e40faf0a6f1dfd2
echizenyayota <virtualmisso30@gmail.com>
2023-12-31 18:56:29 +0900

    1:11:04 Set the image size as 256 * 256 to getVariations

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
   const [error, setError] = useState(null);
+  const ref = useRef(null);
 
   console.log('selectedImage', selectedImage);
   const closeModal = () => {
@@ -9,13 +10,24 @@ const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
     setSelectedImage(null);
   }
 
+  const checkSize = () => {
+    if (ref.current.width === 256 && ref.current.height === 256) {
+      // generateVariations
+
+    } else {
+      setError('Error: Choose 256 * 256 image');
+    }
+  }
+
   return (
     <div className="modal">
       <div onClick={closeModal}>X</div>
-      <div className="img-container">
-        {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="uploaded image"/>}
+      <div className="image-container">
+        {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image"/>}
       </div>
-      <button>Generate</button>
+      <p>{error || "* Image must be 256 * 256"}</p>
+      {!error && <button onClick={checkSize}>Generate</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
     </div>
   );
 }
